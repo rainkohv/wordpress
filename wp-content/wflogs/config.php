8adfc37e434b29bb489b19a4208363e3489495ed
Rain Kohv <rain.kohv@khk.ee>
2020-12-11 12:51:46 +0200

    modified file `wflogs` (after activation of `Wordfence Security` version 7.4.14)

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1608288700;s:7:"authKey";s:64:"`%L>DqD4H6DW)LDZ TL[|`NrPy}c#Gwu&Aa2L3(o^fAWGAIu;UQ2si 9Yl;_ 2/X";}
